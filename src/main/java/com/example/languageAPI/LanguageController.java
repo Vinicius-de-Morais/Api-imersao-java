539653dcfb10474747d2e80bcf2af60412391ca5
vinicius morais <vmorais194@gmail.com>
2022-07-22 00:02:34 -0300

    Creating the Api

---
+++ src/main/java/com/example/languageAPI/LanguageController.java
@@ -0,0 +1,60 @@
+package com.example.languageAPI;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LanguageController {
+    
+    // take of database
+    @Autowired
+    private LanguageRepository repository;
+
+    // get the data
+    @GetMapping("/languages")
+    public List<Language> getLanguages(){
+        List<Language> languages = repository.findAll(Sort.by(Sort.Direction.ASC, "ranking"));
+        return languages;
+    }
+
+    // register an new lang
+    @PostMapping("/languages")
+    public ResponseEntity<Language> postLanguages(@RequestBody Language language){
+        Language savedLang = repository.save(language);
+        return new ResponseEntity<>(savedLang, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("languages/delete/{id}")
+    public ResponseEntity<Optional<Language>> deleteLanguage(@PathVariable("id") String id){
+        var deletedLang = repository.findById(id);
+        repository.deleteById(id);
+        return  new ResponseEntity<>(deletedLang, HttpStatus.OK);
+    }
+
+    @PutMapping("languages/update/{id}")
+    public ResponseEntity<Optional<Language>> updateLanguage(@PathVariable("id") String id){
+        var updatedLang = repository.findById(id);
+        if (updatedLang.isPresent()) {
+            var update = updatedLang.get();
+            update.setTitle(update.getTitle());
+            update.setImage(update.getImage());
+            update.setRanking(update.getRanking());
+            repository.save(update);
+            return new ResponseEntity<>(updatedLang, HttpStatus.OK);
+          } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+          }
+    }
+}
---
+++ src/test/java/com/example/languageAPI/LanguageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.languageAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
